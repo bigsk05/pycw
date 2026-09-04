438b641aa538b731e856530eefdc989a3a859e77
Bigsk <i@ianxia.com>
2026-09-04 14:35:26 +0800

    chore: update version

--- pycw/__version__.py
+++ pycw/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "pycw"
 __description__ = "Generate Morse Code (CW) audio files in Python."
 __url__ = "https://github.com/bigsk05/pycw"
-__version__ = '0.0.3'
+__version__ = '1.1.0'
 __author__ = "Ian Xia"
 __author_email__ = "i@ianxia.com"
 __license__ = "Apache 2.0"
